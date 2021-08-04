cb26bbe0320a6e32e0ff33b9d3f597c4240c9d75
George <george.silva@sciencelogic.com>
2021-08-04 09:50:18 -0300

    Frontend: Add functionalities to click on face button and square buttons

--- client/src/components/Game/component.tsx
+++ client/src/components/Game/component.tsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, {useEffect, useCallback} from 'react'
 import { HeaderComponent } from '../Main/components/component'
+import CountUp from 'react-countup';
 
 export default function GameComponent() {
+
+    const [seconds, setSeconds] = React.useState(0);
+    const ClickEffectFace = useCallback((event) => {
+
+    }, []);
+
+    const ClickEffect = useCallback((event) => {
+        event.preventDefault();
+        let className = event.target.className;
+
+        if (event.type === 'click') {
+            if(className === "value" || className === "mine"){
+                return;
+            }
+            else if(className == "pattern"){
+                event.target.style.display = "none";
+            }
+        } else if (event.type === 'contextmenu') {
+            let element;
+
+            if(className === "pattern"){
+                element = event.target.previousElementSibling;
+            }
+            else if(className === "flag") {
+                element = event.target;
+            }
+            else {
+                return;
+            }
+    
+            if(element.style.display && element.style.display === "block"){
+                element.style.display = "none";
+            }
+            else {
+                element.style.display = "block";
+            }
+        }
+        
+    }, []);
+
+    useEffect(() => {
+        if (seconds < 999) {
+          setTimeout(() => setSeconds(seconds + 1), 1000);
+        } else {
+          setSeconds(0);
+        }
+    });
 
     return (
         <div className="container">
@@ -10,19 +58,19 @@ export default function GameComponent() {
                     <div className="row">
                         <div className="col-md-4">
                             <div className="panel-score-box">
-                                
+                                <span>000</span>
                             </div>
                         </div>
                         <div className="col-md-4">
                             <div className="control-button">
-                                <div className="control-face-normal">
-                                &nbsp;
+                                <div className="control-face control-face-normal">
+                                    &nbsp;
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4">
                             <div className="panel-time-box">
-                                
+                                <span>{seconds}</span>
                             </div>
                         </div>
                     </div>
@@ -31,198 +79,33 @@ export default function GameComponent() {
                     <table className="board">
                         <tr>
                             <td>
-                                <div className="cell">
+                                <div className="cell" 
+                                    onClick={(event) => ClickEffect(event)} 
+                                    onContextMenu={(event) => ClickEffect(event)}
+                                >
                                     <div className="flag">&nbsp;</div>
                                     <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-1">1</span></div>
+                                    <div className="spot-item value">
+                                        <span className="color-adj-1"></span>
+                                    </div>
                                 </div>
                             </td>
                             <td>
-                                <div className="cell">
+                                <div className="cell" 
+                                    onClick={(event) => ClickEffect(event)} 
+                                    onContextMenu={(event) => ClickEffect(event)}
+                                >
                                     <div className="flag">&nbsp;</div>
                                     <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-2">2</span></div>
+                                    <div className="mine">
+                                     &nbsp;
+                                    </div>
                                 </div>
                             </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-3">3</span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-1">1</span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
+                            
+                           
+                           
                         </tr>
-                        
-                        <tr>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-1">1</span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-2">2</span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-3">3</span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-1">1</span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-1">1</span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-2">2</span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-3">3</span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-1">1</span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="mine_red">&nbsp;</div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="mine">&nbsp;</div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className="cell">
-                                    <div className="flag">&nbsp;</div>
-                                    <div className="pattern">&nbsp;</div>
-                                    <div className="value"><span className="color-adj-0"></span></div>
-                                </div>
-                            </td>
-                        </tr>
-
                     </table>
                 </div>
             </div>

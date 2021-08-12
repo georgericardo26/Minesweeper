13d7f747a2a66b4a858ec2fb9a8994047ba79350
George <george.silva@sciencelogic.com>
2021-08-12 10:35:07 -0300

    Backend: Add new ALLOWED_HOST IP

--- server/minesweeper/settings/dev.py
+++ server/minesweeper/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "18.118.1.2"]
+ALLOWED_HOSTS = ["localhost", "3.21.33.18"]
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = ["http://localhost", "http://18.118.1.2"]

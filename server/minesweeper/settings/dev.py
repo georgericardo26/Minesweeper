51f02bd9cb9076d332c50102187a85be25bfecda
George <george.silva@sciencelogic.com>
2021-08-12 20:27:54 -0300

    Backend: Customize dev settings

--- server/minesweeper/settings/dev.py
+++ server/minesweeper/settings/dev.py
@@ -6,5 +6,5 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "3.21.33.18"]
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ["http://localhost", "http://18.118.1.2"]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = []
